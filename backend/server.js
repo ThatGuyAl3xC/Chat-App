5bf695fdb2ea615631e63199354a242a432594ef
Alexandru <allexan93@gmail.com>
2024-07-13 14:26:53 -0700

    Last Part befor Deployment (Socket.io)

--- backend/server.js
+++ backend/server.js
@@ -7,12 +7,11 @@ import messageRoutes from "./routes/message.routes.js";
 import usesrRoutes from "./routes/user.routes.js"; 
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
-
-
-const app = express();
-const PORT = process.env.PORT || 5000;
+import { app, server} from "./socket/socket.js";
  
 dotenv.config();
+
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); 
 app.use(cookieParser());
@@ -22,13 +21,7 @@ app.use("/api/messages", messageRoutes);
 app.use("/api/users", usesrRoutes); 
 
 
-
-// app.get("/", (req, res) => {
-//     res.send("Hello I am ALIVEEEE!!!!!!!")
-// })  
-
-
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     connectToMongoDB();
     console.log(`Server Running on port ${PORT}`);
 });
--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -11,6 +11,7 @@ const Message = ({message}) => {
   const chatClassName = fromMe ? 'chat-end' : 'chat-start';
   const profilePic = fromMe ? authUser.profilePic: selectedConversation?.profilePic;
   const bubbleBgColor = fromMe ? 'bg-orange-400' : "";
+  const shakeClass = message.shouldShake ? "shake" : "";
 
 
   return (
@@ -22,7 +23,7 @@ const Message = ({message}) => {
                 alt="Tailwind CSS chat bubble component" />
             </div>
         </div>
-        <div className={`chat-bubble text-white ${bubbleBgColor} pb-2`} >{message.message}</div>
+        <div className={`chat-bubble text-white ${bubbleBgColor} ${shakeClass} pb-2`} >{message.message}</div>
         <div className="chat-footer opacity-50 text-xs flex gap-1 items-center">{formattedTime}</div>
     </div>
   )
